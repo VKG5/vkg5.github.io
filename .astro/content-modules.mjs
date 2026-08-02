cca0ccb766387fdfb7ce8aecb541a3effd54b426
Lumi_V <bawagupta69@gmail.com>
2026-08-03 02:28:48 +0530

    updated-date

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["src/content/blog/game-automation-01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgame-automation-01.mdx&astroContentModuleFlag=true")],
-["src/content/blog/shaders-procedural-clouds-01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fshaders-procedural-clouds-01.mdx&astroContentModuleFlag=true")],
-["src/content/projects/ai-landscape-generator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fai-landscape-generator.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/ai-landscape-generator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fai-landscape-generator.mdx&astroContentModuleFlag=true")],
+["src/content/blog/shaders-procedural-clouds-01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fshaders-procedural-clouds-01.mdx&astroContentModuleFlag=true")]]);
 		
